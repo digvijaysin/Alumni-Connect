6ace4ebef1594a4323bff0dd15c75e537b659ff6
Digvijay <digvijay.14bcs2008@abes.ac.in>
2017-02-15 23:36:29 -0800

    login complete

---
+++ app/src/main/java/com/example/shivanshu/alumniconnect/StudentDetail.java
@@ -0,0 +1,13 @@
+package com.example.shivanshu.alumniconnect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_detail);
+    }
+}
